1e5e8951b19f413683ccd461bb263674ae92f5df
xavifm <60983549+xavifm@users.noreply.github.com>
2025-04-01 19:53:25 +0200

    tempfiles

--- PartyJungle/PartyJungle/Intermediate/Build/BuildRulesProjects/PartyJungleModuleRules/obj/Development/PartyJungleModuleRules.AssemblyInfo.cs
+++ PartyJungle/PartyJungle/Intermediate/Build/BuildRulesProjects/PartyJungleModuleRules/obj/Development/PartyJungleModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartyJungleModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b8eb8b4eae049101f1181dcc7f20d1060f1d158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dad1302d2e3a26269ba5fb84bc1d16068507e3b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartyJungleModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartyJungleModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
