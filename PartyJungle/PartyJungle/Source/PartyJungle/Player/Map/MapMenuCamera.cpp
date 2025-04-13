ceeec3828dfc97debbd2c10315c606a616520a00
Joel Romero <joel.romero@enti.cat>
2025-04-13 20:04:10 +0200

    asdafagfawq

--- PartyJungle/PartyJungle/Source/PartyJungle/Player/Map/MapMenuCamera.cpp
+++ PartyJungle/PartyJungle/Source/PartyJungle/Player/Map/MapMenuCamera.cpp
@@ -175,7 +175,7 @@ void AMapMenuCamera::SwitchMainScene(int _sceneIndex)
 
 void AMapMenuCamera::SwitchController() 
 {
-    APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), CurrentMinionTeam);
+    APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
     APlayerController* PlayerController0 = UGameplayStatics::GetPlayerController(GetWorld(), 0);
 
 
