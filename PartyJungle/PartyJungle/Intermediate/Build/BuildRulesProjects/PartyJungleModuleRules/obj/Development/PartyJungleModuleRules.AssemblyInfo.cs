185a2d98a028da94523ad8b1aff1ea4d455fbbb4
xavifm <60983549+xavifm@users.noreply.github.com>
2025-05-23 15:48:14 +0200

    added the board as a blueprint with c++ base class

--- PartyJungle/PartyJungle/Intermediate/Build/BuildRulesProjects/PartyJungleModuleRules/obj/Development/PartyJungleModuleRules.AssemblyInfo.cs
+++ PartyJungle/PartyJungle/Intermediate/Build/BuildRulesProjects/PartyJungleModuleRules/obj/Development/PartyJungleModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartyJungleModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b91e0820c7f9959b36f1867b24fac536f27edae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c488d816aeaa252a16eae6c40433e1b16fd8f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartyJungleModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartyJungleModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
