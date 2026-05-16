21928c20e33daf7a909a779a9107206e59cd32e4
JoelRomeroBru <joelromerobru@gmail.com>
2026-05-16 19:32:13 +0200

    Ara si

--- PartyJungle/PartyJungle/Intermediate/Build/BuildRulesProjects/PartyJungleModuleRules/obj/Development/PartyJungleModuleRules.AssemblyInfo.cs
+++ PartyJungle/PartyJungle/Intermediate/Build/BuildRulesProjects/PartyJungleModuleRules/obj/Development/PartyJungleModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartyJungleModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60dce5f840803496aa687c121aeb8b997765f887")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a06303883b654d84088f34acc463363117ef9ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartyJungleModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartyJungleModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
