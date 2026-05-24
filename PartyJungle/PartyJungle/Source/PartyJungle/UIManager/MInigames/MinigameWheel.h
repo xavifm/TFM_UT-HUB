0dae254b51c9c92229b085205fac3a3166fb5c3a
Xavier <xaviflores1@gmail.com>
2026-05-24 16:28:27 +0200

    added a value to minus one

--- PartyJungle/PartyJungle/Source/PartyJungle/UIManager/MInigames/MinigameWheel.h
+++ PartyJungle/PartyJungle/Source/PartyJungle/UIManager/MInigames/MinigameWheel.h
@@ -29,7 +29,7 @@ private:
 	bool Spinning = false;
 	float SpinningTime = 0.0f;
 	int WheelValue = 0;
-	int OldWheelValue = 0;
+	int OldWheelValue = -1;
 	
 	FTimerHandle SpinStepTimerHandle;
 	FTimerHandle SpinStopTimerHandle;
